8996b2007c37638904e118101f66b62d5ecb65bc
Marcus Bergo <eu@mbergo.dev>
2023-05-15 11:38:09 -0300

    Adding RNDC implementation
    This adds a rndc command control over Bind name server

--- src/bind_service.py
+++ src/bind_service.py
@@ -1,21 +1,31 @@
+import subprocess
+
+
 class BINDService:
     def __init__(self):
-        # Initialize the BIND service connection
+        self.rndc_path = '/path/to/rndc'
+
+    def execute_rndc_command(self, command):
+        cmd = [self.rndc_path, '-c', '/path/to/named.conf'] + command
+        subprocess.run(cmd)
 
     def create_domain(self, domain_name):
-        # Implement creating a domain using RNDC
+        self.execute_rndc_command(
+            ['addzone', domain_name, '{ type master; file "/path/to/zone.db"; };'])
 
     def update_domain(self, domain_name):
-        # Implement updating a domain using RNDC
+        self.execute_rndc_command(['modzone', domain_name, 'refresh'])
 
     def delete_domain(self, domain_name):
-        # Implement deleting a domain using RNDC
+        self.execute_rndc_command(['delzone', domain_name])
 
     def create_record(self, domain_name, record_name, record_value):
-        # Implement creating a record using RNDC
+        self.execute_rndc_command(
+            ['add', domain_name, 'IN', 'A', record_value])
 
     def update_record(self, domain_name, record_name, record_value):
-        # Implement updating a record using RNDC
+        self.execute_rndc_command(
+            ['mod', domain_name, 'IN', 'A', record_value])
 
     def delete_record(self, domain_name, record_name):
-        # Implement deleting a record using RNDC
+        self.execute_rndc_command(['del', domain_name, 'IN', 'A', record_name])
